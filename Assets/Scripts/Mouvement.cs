4fd92942323e8c1fd595aab46102578a8217dad9
Nils <nils.dargent@gmail.com>
2026-01-06 14:32:23 +0100

    Add Walk, Idle, Jump and Disk animation

---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouvement : MonoBehaviour
+{
+    [SerializeField] 
+    private float MoveSpeed = 5f;
+    [SerializeField]
+    private float JumpForce = 10f;
+    
+
+    private SpriteRenderer _sprite;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private float _moveInput;
+    private bool _wantsToJump;
+
+
+    [Header("RaycastController")]
+    [SerializeField]
+    private float PlayerHeight = 0.7f;
+    [SerializeField]
+    private LayerMask GroundMask;
+
+    void Start()
+    {
+
+        _rb = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool onGround = Physics2D.Raycast(transform.position, Vector2.down, PlayerHeight, GroundMask);
+        _moveInput = Input.GetAxis("Horizontal");
+        if (_moveInput != 0)
+        {
+            if (onGround)
+            {
+                _animator.SetBool("isRunning", true);
+            }
+            else
+            {
+                _animator.SetBool("isRunning", false);
+            }
+            if (_moveInput < 0)
+            {
+                _sprite.flipX = true;
+            }
+            else if (_moveInput > 0)
+            {
+                _sprite.flipX = false;
+            }
+
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+
+        if (onGround)
+        {
+            _animator.SetBool("isJumping", false);
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                _wantsToJump = true;
+                _animator.SetBool("isJumping", true);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = new Vector2(_moveInput * MoveSpeed, _rb.linearVelocityY);
+        if (_wantsToJump)
+        {
+            _wantsToJump = false;
+
+            _rb.AddForceY(JumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
