b41bf9b3245fa91055edd5775efcc77ee838cebb
Nils <nils.dargent@gmail.com>
2026-01-08 14:03:34 +0100

    add a level, enemies and there animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    public Transform[] waypoints;
+
+    private SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        graphics = GetComponent<SpriteRenderer>();
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        //si lenemi est quasi arrivé à sa destination
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
