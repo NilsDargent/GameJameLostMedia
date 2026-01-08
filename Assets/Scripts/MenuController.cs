db49e7cdc2383c7de817f225540236ab4ebd9ce2
Nils <nils.dargent@gmail.com>
2026-01-08 14:19:30 +0100

    add logo and title screen sprite, and organise all sprites assets

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,9 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject option;
+
     public void OnStart()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("Credit").name);
+    }
+    public void Options()
+    {
+        option.SetActive(true);
+
     }
 
     public void Quit()
